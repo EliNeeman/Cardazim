2f9a0041b5b7f33911e942ccf7af16d6666de38a
Eli <eli@neeman.com>
2026-09-14 15:19:57 +0300

    Added Threading to server

--- cardazim/server.py
+++ cardazim/server.py
@@ -6,7 +6,7 @@ import sys
 ###########################################################
 import socket
 import struct
-
+import threading
 def run_server(server_ip, server_port):
     '''
     Run a server in address (server_ip, server_port)
@@ -16,11 +16,17 @@ def run_server(server_ip, server_port):
             s.bind((server_ip, server_port))
             s.listen(1)
             conn, addr = s.accept()
-            with conn:
-                packed_data  = conn.recv(1024)
-                data_size = struct.unpack('<i',packed_data[:4])[0]
-                data = struct.unpack(f'<i{data_size}s',packed_data)[1].decode()
-                print(f'Recived data: {data}')
+            t = threading.Thread(target=handle_connection,args=(conn,))
+            t.start()
+            t.join()
+
+def handle_connection(conn):
+    with conn:
+        packed_data  = conn.recv(1024)
+        data_size = struct.unpack('<i',packed_data[:4])[0]
+        data = struct.unpack(f'<i{data_size}s',packed_data)[1].decode()
+        print(f'Recived data: {data}')
+
             
 
 
